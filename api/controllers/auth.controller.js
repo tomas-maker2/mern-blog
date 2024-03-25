6b711a21deec9ffe21c6cf147ad40e1e8f23f2ca
tomas-maker2 <tbolotnicoff@gmail.com>
2024-03-25 12:22:05 -0300

    create sign up api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,30 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ message: "All Fields are required" });
+  }
+  const hashPassword = bcryptjs.hashSync(password,10)
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json("Sign up ok");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
