b1ffb8972d68ea6fb3568e515bc4d17669a44ac1
tomas-maker2 <tbolotnicoff@gmail.com>
2024-03-25 11:31:28 -0300

    connect to Mongo

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("MongoDB OK"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
+app.listen(3000, () => console.log("Server OK"));
 
-app.listen(3000, ()=> console.log("Server OK"))
+// GbesrntzVG0oC2Bi
