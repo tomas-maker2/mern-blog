dfd7b201ff3dea50669b03f9857703254dbd35fd
tomas-maker2 <tbolotnicoff@gmail.com>
2024-03-27 09:56:15 -0300

    make dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,30 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import DashBoard from './pages/DashBoard';
-import Projects from './pages/Projects';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import DashBoard from "./pages/DashBoard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
-   <BrowserRouter>
-   <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/sign-in' element={<Signin/>}/>
-      <Route path='/sign-up' element={<Signup/>}/>
-      <Route path='/dashboard' element={<DashBoard/>}/>
-      <Route path='/projects' element={<Projects/>}/>
-    </Routes>
-    <Footer/>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<DashBoard />} />
+        </Route>
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
