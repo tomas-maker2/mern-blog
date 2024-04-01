f8950dbbd6354df0963c160eb1b3353a59b7ce86
tomas-maker2 <tbolotnicoff@gmail.com>
2024-04-01 10:34:25 -0300

    add post api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import postRouter from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -17,10 +18,11 @@ mongoose
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/post", postRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
