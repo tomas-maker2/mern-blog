cf407c460eb66689d7e16984c04d58b45d71c5ab
tomas-maker2 <tbolotnicoff@gmail.com>
2024-04-02 10:14:15 -0300

    update func sin resolver

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create, deletepost, getposts } from '../controllers/post.controller.js';
+import { create, deletepost, getposts, updatepost } from '../controllers/post.controller.js';
 
 
 
@@ -11,5 +11,7 @@ router.post('/create', verifyToken, create)
 router.get('/getposts', getposts)
 
 router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
+
+router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
 
 export default router
