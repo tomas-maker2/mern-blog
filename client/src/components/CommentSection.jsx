0e3dfbdbc336bbf62376cbfb502f224b003458de
tomas-maker2 <tbolotnicoff@gmail.com>
2024-04-03 18:21:35 -0300

    add like func com

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Comment from "./Comment";
+import { useNavigate } from "react-router-dom";
 
 export default function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
@@ -12,6 +13,8 @@ export default function CommentSection({ postId }) {
   const [comments, setComments] = useState([]);
 
   const [commentError, setCommentError] = useState(null);
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -34,7 +37,7 @@ export default function CommentSection({ postId }) {
       if (res.ok) {
         setComment("");
         setCommentError(null);
-        setComments([data, ...comments])
+        setComments([data, ...comments]);
       }
     } catch (error) {
       setCommentError(error.message);
@@ -55,6 +58,34 @@ export default function CommentSection({ postId }) {
     };
     getComments();
   }, [postId]);
+
+  const hadleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate("/sign-in");
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: "PUT",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
@@ -112,15 +143,15 @@ export default function CommentSection({ postId }) {
         <p className="text-sm my-5">No comments yet</p>
       ) : (
         <>
-        <div className="text-sm my-5 flex items-center gap-1">
-          <p>Comments</p>
-          <div className="border border-gray-400 py-1 px-2 rounded-sm">
-            <p>{comments.length}</p>
+          <div className="text-sm my-5 flex items-center gap-1">
+            <p>Comments</p>
+            <div className="border border-gray-400 py-1 px-2 rounded-sm">
+              <p>{comments.length}</p>
+            </div>
           </div>
-        </div>
-        {comments.map(comment => (
-          <Comment key={comment._id} comment={comment}/>
-        ))}
+          {comments.map((comment) => (
+            <Comment key={comment._id} comment={comment} onLike={hadleLike} />
+          ))}
         </>
       )}
     </div>
