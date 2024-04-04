792df97ba5f4c7ec39920b81e58edf7d3a70e83e
tomas-maker2 <tbolotnicoff@gmail.com>
2024-04-04 10:28:34 -0300

    add recent article section to postpage

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,12 +3,14 @@ import { Link, useParams } from "react-router-dom";
 import { Button, Spinner } from "flowbite-react";
 import CallToAction from "../components/CallToAction";
 import CommentSection from "../components/CommentSection.jsx";
+import PostCard from "../components/PostCard.jsx";
 
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null)
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -33,6 +35,22 @@ export default function PostPage() {
     };
     fetchPost();
   }, [postSlug]);
+
+  useEffect(() => {
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  },[])
+
 
   if (loading) {
     <div className="flex justify-center items-center min-h-screen">
@@ -73,6 +91,19 @@ export default function PostPage() {
       </div>
       {/* Verificar si post no es null antes de intentar acceder a post._id */}
       {post && <CommentSection postId={post._id} />}
+      
+      
+      <div className="flex flex-col justify-center items-center mb-5">
+        <h1 className="text-xl mt-5 ">Recent Articles</h1>
+        <div className="flex flex-wrap gap-5 mt-5 justify-center">
+        {
+          recentPosts &&
+            recentPosts.map((post)=> (
+              <PostCard key={post._id} post={post}/>
+            ))
+        }
+        </div>
+      </div>
     </main>
   );
 }
