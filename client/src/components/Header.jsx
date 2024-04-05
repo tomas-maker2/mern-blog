a134019406c46398af9d73eae0858b249021c66c
tomas-maker2 <tbolotnicoff@gmail.com>
2024-04-05 13:46:15 -0300

    complete search page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon , FaSun} from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
@@ -9,9 +9,21 @@ import { signoutSuccess } from "../redux/user/userSlice";
 
 const Header = () => {
   const path = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate()
   const { currentUser } = useSelector((state) => state.user);
   const dispatch = useDispatch()
   const {theme} = useSelector((state) => state.theme)
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl)
+    }
+  },[location.search])
+
   const handleSignOut = async(e) => {
     try {
       const res = await fetch(`/api/user/signout`,{
@@ -27,6 +39,14 @@ const Header = () => {
       console.log(error.message);
     }
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set("searchTerm", searchTerm)
+    const searchQuery = urlParams.toString()
+    navigate(`/search?${searchQuery}`)
+  }
   return (
     <Navbar className="border-b-2">
       <Link
@@ -38,12 +58,14 @@ const Header = () => {
         </span>
         Blog
       </Link>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextInput
           type="text"
           placeholder="Search..."
           rightIcon={AiOutlineSearch}
           className="hidden lg:inline"
+          value={searchTerm}
+          onChange={(e)=> setSearchTerm(e.target.value)}
         />
       </form>
       <Button className="w-12 h10 lg:hidden" color={"gray"} pill>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,16 +4,16 @@ import { useEffect, useState } from "react";
 import PostCard from "../components/PostCard";
 
 const Home = () => {
-  const [posts,setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await fetch('/api/post/getPosts')
-      const data  = await res.json()
-      setPosts(data.posts)
-    }
-    fetchPosts()
-  }, [])
+      const res = await fetch("/api/post/getPosts");
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
   return (
     <div>
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
@@ -22,31 +22,33 @@ const Home = () => {
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati,
           possimus
         </p>
-      <Link
-        to={"/search"}
-        className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
-      >
-        View All Posts
-      </Link>
+        <Link
+          to={"/search"}
+          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
+        >
+          View All Posts
+        </Link>
       </div>
       <div className="p-3 bg-amber-100 dark:bg-slate-700">
-        <CallToAction/>
+        <CallToAction />
       </div>
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
-        {
-          posts && posts.length > 0 && (
-            <div className="flex flex-col gap-6">
-              <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
-              <div  className="flex flex-wrap gap-4 justify-center">
-                {posts.map((post)=> (
-                  <PostCard post={post} key={post._id}/>
-                ))}
-              </div>
-              <Link to={'/search'} className="text-lg text-teal-500 hover:underline text-center">
-              View all post</Link>
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
+            <div className="flex flex-wrap gap-4 justify-center">
+              {posts.map((post) => (
+                <PostCard post={post} key={post._id} />
+              ))}
             </div>
-          )
-        }
+            <Link
+              to={"/search"}
+              className="text-lg text-teal-500 hover:underline text-center"
+            >
+              View all post
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
